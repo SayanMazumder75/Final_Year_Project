7ccd0e54e3ef706adf3354184166f0d102658885
anushka668 <anushkadutta2580@gmail.com>
2025-10-21 22:59:21 +0530

    Update App.jsx

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -18,7 +18,7 @@ import AdminSignup from "./Login_Signup/AdminSignup";
 import AdminLogin from "./Login_Signup/AdminLogin";
 import Wishlist from "./User_Dashboard/Wishlist";
 import PostAd from "./Admin_Panel/PostAd";
-import Wishlist from "./User_Dashboard/Wishlist";
+
 
 function App() {
   const [loading, setLoading] = useState(true);
